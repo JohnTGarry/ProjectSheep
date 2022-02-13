1769eb0f0b2199cf75b1d4ff9877455b643453ce
RivalGarry <johntgarry@gmail.com>
2022-02-13 20:02:43 +0000

    Importing temp assets

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private CharacterController controller;
+        [SerializeField] private float runSpeed = 40f;
+
+        private float horizontalMove = 0f;
+        private bool jump = false;
+
+        void Update()
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+            if  (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+            jump = false;
+        }
+    }
+}
